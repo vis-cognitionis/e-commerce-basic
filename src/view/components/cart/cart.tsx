53716b524eefd1871b6f79a9b471e409ad8e6ea1
Vis Cognitionis <omeralemdar94@gmail.com>
2023-04-27 00:24:00 +0300

    Update cart.tsx

--- src/view/components/cart/cart.tsx
+++ src/view/components/cart/cart.tsx
@@ -1,8 +1,47 @@
 import React from "react";
+import {
+  Box,
+  Button,
+  Stack,
+  StackProps,
+  ButtonProps,
+  styled,
+  BoxProps,
+} from "@mui/material";
+
 import ContainerCard from "core/components/cards/container_card";
-import { Box, Button, Stack } from "@mui/material";
 import Text from "core/components/typography/typography";
-import { colors } from "../../../core/contants/colors";
+import { colors } from "core/contants/colors";
+
+const CartContainer = styled(Stack)<StackProps>(() => ({
+  maxHeight: "120px",
+  overflowY: "auto",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+}));
+
+const CountButton = styled(Button)<ButtonProps>(() => ({
+  width: "25px",
+  maxWidth: "25px",
+  minWidth: "25px",
+  padding: 0,
+  height: "25px",
+  borderRadius: "4px",
+  backgroundColor: colors.lightGray,
+  color: colors.textDark,
+}));
+
+const QuantityBox = styled(Box)<BoxProps>(() => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "2.5px 10.5px 2.5px 10.5px",
+  width: "27px",
+  height: "27px",
+  backgroundColor: colors.primary,
+  boxSizing: "border-box",
+}));
 
 const Cart = () => {
   return (
@@ -10,76 +49,41 @@ const Cart = () => {
       sx={{ width: "213px", height: "auto", padding: "10px" }}
       isFilter={false}
       children={
-        <Stack>
-          <Stack
-            direction="row"
-            spacing={2}
-            alignItems={"center"}
-            width={"193px"}
-          >
-            <Stack width={"106px"}>
-              <Text variant="caption" content="Samsung S22" />
-              <Text
-                content="12.000 ₺"
-                sx={{
-                  color: colors.primary,
-                  mt: "-4px",
-                  fontSize: 10,
-                  fontWeight: 500,
-                }}
-              />
-            </Stack>
+        <CartContainer spacing={1.8} pb={1.5}>
+          {Array(10).fill(
             <Stack
               direction="row"
-              width={"78px"}
-              height={"27px"}
+              spacing={2}
               alignItems={"center"}
+              width={"193px"}
             >
-              <Button
-                sx={{
-                  width: "25px",
-                  maxWidth: "25px",
-                  minWidth: "25px",
-                  padding: 0,
-                  height: "25px",
-                  borderRadius: "4px",
-                  backgroundColor: colors.lightGray,
-                  color: colors.textDark,
-                }}
+              <Stack width={"106px"}>
+                <Text variant="caption" content="Samsung S22" />
+                <Text
+                  content="12.000 ₺"
+                  sx={{
+                    color: colors.primary,
+                    mt: "-4px",
+                    fontSize: 10,
+                    fontWeight: 500,
+                  }}
+                />
+              </Stack>
+              <Stack
+                direction="row"
+                width={"78px"}
+                height={"27px"}
+                alignItems={"center"}
               >
-                -
-              </Button>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding: "2.5px 10.5px 2.5px 10.5px",
-                  width: "27px",
-                  height: "27px",
-                  backgroundColor: colors.primary,
-                  boxSizing: "border-box",
-                }}
-              >
-                <Text content="2" sx={{ color: colors.textLight }} />
-              </Box>
-              <Button
-                sx={{
-                  width: "25px",
-                  maxWidth: "25px",
-                  minWidth: "25px",
-                  padding: 0,
-                  height: "25px",
-                  borderRadius: "4px",
-                  backgroundColor: colors.lightGray,
-                  color: colors.textDark,
-                }}
-              >
-                +
-              </Button>
+                <CountButton>-</CountButton>
+                <QuantityBox>
+                  <Text content="2" sx={{ color: colors.textLight }} />
+                </QuantityBox>
+                <CountButton>+</CountButton>
+              </Stack>
             </Stack>
-          </Stack>
-        </Stack>
+          )}
+        </CartContainer>
       }
     />
   );
