fb51f78c5f526558a9e39f5ef0c5ab3b79649c9c
Vis Cognitionis <omeralemdar94@gmail.com>
2023-04-26 22:21:56 +0300

    Add custom-hook

---
+++ src/custom-hook/usePaginatedProducts.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import useGetProducts from "service/useGetProducts";
+import mainStore from "view-model/main_store";
+
+export const usePaginatedProducts = () => {
+  const { products } = useGetProducts();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const itemsPerPage = 12;
+
+  //extracts the page parameter from the URL or sets it to 1 to get the page number
+  const page = parseInt(
+    new URLSearchParams(location.search).get("page") || "1"
+  );
+
+  const [currentPage, setCurrentPage] = useState<number>(page);
+
+  //updates the current page number via the setCurrentPage hook and also updates the page number in the URL
+  const handleChangePage = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    setCurrentPage(page);
+    navigate(`/?page=${page}`);
+  };
+
+  //All products from the data are firstly filtered here
+  //These filter parameters are the product brand, model and search for the product name by search component
+  //filters.length === 0 is required to display all products if no filters are selected
+  let filteredProductList = products?.filter((product) => {
+    const filters = mainStore.selectedFilters;
+    const searchName = mainStore.searchName.toLowerCase();
+    return (
+      (filters.length === 0 ||
+        filters.some(
+          (filter) => product.brand === filter || product.model === filter
+        )) &&
+      (searchName === "" || product.name.toLowerCase().includes(searchName))
+    );
+  });
+
+  //required to sort products according to the values of sort by component
+  switch (mainStore.selectedItem) {
+    case "price-low-to-high":
+      filteredProductList = filteredProductList?.sort(
+        (a, b) => Number(a.price) - Number(b.price)
+      );
+      break;
+    case "price-high-to-low":
+      filteredProductList = filteredProductList?.sort(
+        (a, b) => Number(b.price) - Number(a.price)
+      );
+      break;
+    case "new-to-old":
+      filteredProductList = filteredProductList?.sort((a, b) =>
+        a.createdAt < b.createdAt ? 1 : -1
+      );
+      break;
+    case "old-to-new":
+      filteredProductList = filteredProductList?.sort((a, b) =>
+        a.createdAt > b.createdAt ? 1 : -1
+      );
+      break;
+    default:
+      break;
+  }
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  //get the products that should be displayed on the current page
+  const paginatedProductList = filteredProductList?.slice(startIndex, endIndex);
+
+  //calculate the total number of pages based on the number of products and items per page
+  const pageCount = Math.ceil(
+    ((filteredProductList && filteredProductList.length) || 0) / itemsPerPage
+  );
+
+  //required to update pagination by URL when doing back and forward in browser
+  useEffect(() => {
+    if (page <= pageCount) {
+      setCurrentPage(page);
+    }
+  }, [location.search, pageCount]);
+
+  const isProductNotFound =
+    paginatedProductList && paginatedProductList.length === 0;
+
+  return {
+    paginatedProductList,
+    pageCount,
+    currentPage,
+    handleChangePage,
+    itemsPerPage,
+    isProductNotFound,
+  };
+};
--- src/view/components/product-cards/product_cards.tsx
+++ src/view/components/product-cards/product_cards.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Box,
   BoxProps,
@@ -14,9 +14,9 @@ import { observer } from "mobx-react";
 import ProductCard from "core/components/cards/product_card";
 import useGetProducts from "service/useGetProducts";
 import LazyLoadingSkeleton from "core/components/lazy-loading/lazy_loading";
-import mainStore from "view-model/main_store";
-import { colors } from "core/contants/colors";
 import Text from "core/components/typography/typography";
+import { usePaginatedProducts } from "custom-hook/usePaginatedProducts";
+import { colors } from "core/contants/colors";
 
 const ProductCardsContainer = styled(Box)<BoxProps>(() => ({
   display: "flex",
@@ -68,79 +68,17 @@ const CustomPagination = styled(Pagination)<PaginationProps>(() => ({
 }));
 
 const ProductCards = () => {
-  const { products, isLoading } = useGetProducts();
+  const {
+    paginatedProductList,
+    pageCount,
+    currentPage,
+    handleChangePage,
+    itemsPerPage,
+    isProductNotFound,
+  } = usePaginatedProducts();
+
   const navigate = useNavigate();
-  const location = useLocation();
-
-  const itemsPerPage = 12;
-
-  const page = parseInt(
-    new URLSearchParams(location.search).get("page") || "1"
-  );
-
-  const [currentPage, setCurrentPage] = useState<number>(page);
-
-  const handleChangePage = (
-    event: React.ChangeEvent<unknown>,
-    page: number
-  ) => {
-    setCurrentPage(page);
-    navigate(`/?page=${page}`);
-  };
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-
-  let filteredProducts = products?.filter((product) => {
-    const filters = mainStore.selectedFilters;
-    const searchName = mainStore.searchName.toLowerCase();
-    return (
-      (filters.length === 0 ||
-        filters.some(
-          (filter) => product.brand === filter || product.model === filter
-        )) &&
-      (searchName === "" || product.name.toLowerCase().includes(searchName))
-    );
-  });
-
-  switch (mainStore.selectedItem) {
-    case "price-low-to-high":
-      filteredProducts = filteredProducts?.sort(
-        (a, b) => Number(a.price) - Number(b.price)
-      );
-      break;
-    case "price-high-to-low":
-      filteredProducts = filteredProducts?.sort(
-        (a, b) => Number(b.price) - Number(a.price)
-      );
-      break;
-    case "new-to-old":
-      filteredProducts = filteredProducts?.sort((a, b) =>
-        a.createdAt < b.createdAt ? 1 : -1
-      );
-      break;
-    case "old-to-new":
-      filteredProducts = filteredProducts?.sort((a, b) =>
-        a.createdAt > b.createdAt ? 1 : -1
-      );
-      break;
-    default:
-      break;
-  }
-
-  const paginatedProducts = filteredProducts?.slice(startIndex, endIndex);
-
-  const pageCount = Math.ceil(
-    ((filteredProducts && filteredProducts.length) || 0) / itemsPerPage
-  );
-
-  useEffect(() => {
-    if (page <= pageCount) {
-      setCurrentPage(page);
-    }
-  }, [location.search, pageCount]);
-
-  const isProductNotFound = paginatedProducts && paginatedProducts.length === 0;
+  const { isLoading } = useGetProducts();
 
   return isProductNotFound ? (
     <Text
@@ -156,8 +94,8 @@ const ProductCards = () => {
             ? Array(itemsPerPage).fill(
                 <LazyLoadingSkeleton width={180} height={302} />
               )
-            : paginatedProducts &&
-              paginatedProducts.map((product) => {
+            : paginatedProductList &&
+              paginatedProductList.map((product) => {
                 return (
                   <ProductCard
                     key={product.id}
@@ -177,7 +115,8 @@ const ProductCards = () => {
                 );
               })}
         </ProductCardsContainer>
-        {!isLoading && (
+
+        {!isLoading && pageCount > 1 && (
           <PaginationContainer>
             <CustomPagination
               count={pageCount}
